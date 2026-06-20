a8bede04931d8f51a999fef77427a7e37f66a9f4
Vivek Jadhav <jadvivr@amazon.com>
2026-06-20 17:23:24 +0530

    feat: add model versioning to fct_customer_lifetime_value (v1 deprecated, v2 with new metrics)

---
+++ models/marts/fct_customer_lifetime_value_v2.sql
@@ -0,0 +1,22 @@
+with orders as (
+    select * from {{ ref('int_orders_enriched') }}
+)
+
+select
+    customer_key,
+    customer_name,
+    customer_nation,
+    customer_region,
+    market_segment,
+    count(distinct order_key) as total_orders,
+    sum(revenue) as lifetime_revenue,
+    sum(profit) as lifetime_profit,
+    min(order_year) as first_order_year,
+    max(order_year) as last_order_year,
+    max(order_year) - min(order_year) + 1 as years_active,
+    sum(revenue) / nullif(count(distinct order_key), 0) as avg_order_value,
+    count(distinct product_category) as categories_purchased,
+    avg(discount_pct) as avg_discount_pct,
+    count(distinct supplier_key) as unique_suppliers
+from orders
+group by 1, 2, 3, 4, 5
